a4d6eeca649d9a9a144ccb2e13c20d17acef54c9
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-17 16:28:40 -0300

    feat(screen - startgame): initial creation of the mini game

---
+++ components/PrimaryButton.js
@@ -0,0 +1,48 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+
+export function PrimaryButton({ children, onPress }) {
+
+  return (
+
+    <View style={styles.buttonContainer}>
+      <Pressable
+        onPress={onPress}
+        android_ripple={{ color: '#640233' }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden'
+  },
+
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75
+  }
+
+});
---
+++ screens/StartGame.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { PrimaryButton } from "../components/PrimaryButton";
+
+export function StartGameScreen() {
+
+  const [enteredNumber, setEnteredNumber] = useState('');
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText)
+  }
+
+  function confirmInputHandler() {
+
+  }
+
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <PrimaryButton
+            onPress={confirmInputHandler}
+          >Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#72063c',
+    borderRadius: 0,
+    elevation: 4,
+
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+
+  buttonsContainer: {
+    flexDirection: "row"
+
+  },
+  buttonContainer: {
+    flex: 1,
+  }
+});
