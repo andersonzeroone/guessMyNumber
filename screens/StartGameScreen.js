7abaa4a80c143b728657e916b31dd456f27b9361
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-19 10:37:34 -0300

    feat(screen - startgame, gameover, app): adapting screen according to rotation and add status bar

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,111 +1,166 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
-  TextInput,
   View,
   StyleSheet,
   Alert,
+  FlatList,
   useWindowDimensions,
-  KeyboardAvoidingView,
-  ScrollView,
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-import PrimaryButton from '../components/ui/PrimaryButton';
-import Title from '../components/ui/Title';
-import Colors from '../constants/colors';
+import NumberContainer from '../components/game/NumberContainer';
 import Card from '../components/ui/Card';
 import InstructionText from '../components/ui/InstructionText';
+import PrimaryButton from '../components/ui/PrimaryButton';
+import Title from '../components/ui/Title';
+import GuessLogItem from '../components/game/GuessLogItem';
 
-function StartGameScreen({ onPickNumber }) {
-  const [enteredNumber, setEnteredNumber] = useState('');
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
 
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function GameScreen({ userNumber, onGameOver }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  const [guessRounds, setGuessRounds] = useState([initialGuess]);
   const { width, height } = useWindowDimensions();
 
-  function numberInputHandler(enteredText) {
-    setEnteredNumber(enteredText);
-  }
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver(guessRounds.length);
+    }
+  }, [currentGuess, userNumber, onGameOver]);
 
-  function resetInputHandler() {
-    setEnteredNumber('');
-  }
+  useEffect(() => {
+    minBoundary = 1;
+    maxBoundary = 100;
+  }, []);
 
-  function confirmInputHandler() {
-    const chosenNumber = parseInt(enteredNumber);
-
-    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
-      Alert.alert(
-        'Invalid number!',
-        'Number has to be a number between 1 and 99.',
-        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
-      );
+  function nextGuessHandler(direction) {
+    // direction => 'lower', 'greater'
+    if (
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'greater' && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", 'You know that this is wrong...', [
+        { text: 'Sorry!', style: 'cancel' },
+      ]);
       return;
     }
 
-    onPickNumber(chosenNumber);
+    if (direction === 'lower') {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+    setGuessRounds((prevGuessRounds) => [newRndNumber, ...prevGuessRounds]);
   }
 
-  const marginTopDistance = height < 380 ? 30 : 100;
+  const guessRoundsListLength = guessRounds.length;
+
+  let content = (
+    <>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card>
+        <InstructionText style={styles.instructionText}>
+          Higher or lower?
+        </InstructionText>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Ionicons name="md-remove" size={24} color="white" />
+            </PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+              <Ionicons name="md-add" size={24} color="white" />
+            </PrimaryButton>
+          </View>
+        </View>
+      </Card>
+    </>
+  );
+
+  if (width > 500) {
+    content = (
+      <>
+        <View style={styles.buttonsContainerWide}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Ionicons name="md-remove" size={24} color="white" />
+            </PrimaryButton>
+          </View>
+          <NumberContainer>{currentGuess}</NumberContainer>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+              <Ionicons name="md-add" size={24} color="white" />
+            </PrimaryButton>
+          </View>
+        </View>
+      </>
+    );
+  }
 
   return (
-    <ScrollView style={styles.screen}>
-      <KeyboardAvoidingView style={styles.screen} behavior="position">
-        <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
-          <Title>Guess My Number</Title>
-          <Card>
-            <InstructionText>Enter a Number</InstructionText>
-            <TextInput
-              style={styles.numberInput}
-              maxLength={2}
-              keyboardType="number-pad"
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={numberInputHandler}
-              value={enteredNumber}
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {content}
+      <View style={styles.listContainer}>
+        {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
+        <FlatList
+          data={guessRounds}
+          renderItem={(itemData) => (
+            <GuessLogItem
+              roundNumber={guessRoundsListLength - itemData.index}
+              guess={itemData.item}
             />
-            <View style={styles.buttonsContainer}>
-              <View style={styles.buttonContainer}>
-                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-              </View>
-              <View style={styles.buttonContainer}>
-                <PrimaryButton onPress={confirmInputHandler}>
-                  Confirm
-                </PrimaryButton>
-              </View>
-            </View>
-          </Card>
-        </View>
-      </KeyboardAvoidingView>
-    </ScrollView>
+          )}
+          keyExtractor={(item) => item}
+        />
+      </View>
+    </View>
   );
 }
 
-export default StartGameScreen;
-
-// const deviceHeight = Dimensions.get('window').height;
+export default GameScreen;
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-  },
-  rootContainer: {
-    flex: 1,
-    // marginTop: deviceHeight < 380 ? 30 : 100,
+    padding: 24,
     alignItems: 'center',
   },
-  numberInput: {
-    height: 50,
-    width: 50,
-    fontSize: 32,
-    borderBottomColor: Colors.accent500,
-    borderBottomWidth: 2,
-    color: Colors.accent500,
-    marginVertical: 8,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  instructionText: {
+    marginBottom: 12,
   },
   buttonsContainer: {
     flexDirection: 'row',
   },
   buttonContainer: {
     flex: 1,
+  },
+  buttonsContainerWide: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
   },
 });
