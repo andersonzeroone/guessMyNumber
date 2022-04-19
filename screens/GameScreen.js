e4a8395b44bcaa569370eb4bd0e23b6e54ecb863
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-19 00:29:36 -0300

    feat(screen - gameover): initial creation of the logic to reset matches

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/game/NumberContainer';
 import Card from '../components/ui/Card';
@@ -68,12 +68,12 @@ function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
-              <Ionicons name='md-remove' size={24} />
+              <Ionicons name="md-remove" size={24} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
-              <Ionicons name='md-add' size={24} />
+            <Ionicons name="md-add" size={24} color="white" />
             </PrimaryButton>
           </View>
         </View>
